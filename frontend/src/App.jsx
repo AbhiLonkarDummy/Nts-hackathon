f4fae75b17689b40d10fbffd4695bbb690a64997
Abhishek Lonkar <abhilonkardummy@outlook.com>
2025-08-28 12:52:06 +0630

    Added some frontend files to test it out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 // Shadcn happens to work really well
-
+// Added some dummy frontend code
 function App() {
   return (
     <>
       <div className="flex flex-col gap-2 min-h-svh  items-center justify-center">
         <Button>Click me</Button>
         <Button>Go here</Button>
+        <Button>The dummy code happens to go in here</Button>
       </div>
     </>
   );
